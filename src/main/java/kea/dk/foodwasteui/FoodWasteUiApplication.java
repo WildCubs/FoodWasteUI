d260f875775c0848dd3c012fdda3f2a573448a53
WildCubs <phil379c@stud.kea.dk>
2022-09-22 09:04:00 +0200

    Trying to get started

---
+++ src/main/java/kea/dk/foodwasteui/FoodWasteUiApplication.java
@@ -0,0 +1,13 @@
+package kea.dk.foodwasteui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWasteUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodWasteUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/dk/foodwasteui/FoodWasteUiApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dk.foodwasteui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWasteUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
